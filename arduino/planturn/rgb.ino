483e76f55a144fd04016456378de1cec66f93ff9
Sascha T. Ishikawa <sascha@zooniverse.org>
2022-10-17 23:49:11 -0700

    add fading rgb

--- arduino/planturn/rgb.ino
+++ arduino/planturn/rgb.ino
@@ -1,46 +1,33 @@
-#define R_PIN 10
-#define G_PIN 11
-#define B_PIN 12
-#define RGB_SPEED 1
- 
-void setup() {
-  pinMode(R_PIN, OUTPUT);
-  pinMode(G_PIN, OUTPUT);
-  pinMode(B_PIN, OUTPUT);
+#include <Fadinglight.h>
+
+#define R_PIN 5
+#define G_PIN 6
+#define B_PIN 9
+#define RGB_INTERVAL 10
+
+Fadinglight ledR(R_PIN, true, 3);
+Fadinglight ledG(G_PIN, true, 6);
+Fadinglight ledB(B_PIN, true, 9);
+
+SpeedSetting rgbSettings = {
+    .on_ms = 800,
+    .off_ms = 800,
+    .pause_ms = 1600,
+    .ending_ms = 3200,
+};
+
+void setupRGB() {
+  ledR.setSpeed(rgbSettings);
+  ledG.setSpeed(rgbSettings);
+  ledB.setSpeed(rgbSettings);
+
+  ledR.off();
+  ledG.blink();
+  ledB.off();
 }
- 
- 
-void loop() {
-  int r, g, b;
- 
-  // fade from blue to violet
-  for (r = 0; r < 256; r++) { 
-    analogWrite(R_PIN, r);
-    delay(RGB_SPEED);
-  } 
-  // fade from violet to red
-  for (b = 255; b > 0; b--) { 
-    analogWrite(B_PIN, b);
-    delay(RGB_SPEED);
-  } 
-  // fade from red to yellow
-  for (g = 0; g < 256; g++) { 
-    analogWrite(G_PIN, g);
-    delay(RGB_SPEED);
-  } 
-  // fade from yellow to green
-  for (r = 255; r > 0; r--) { 
-    analogWrite(R_PIN, r);
-    delay(RGB_SPEED);
-  } 
-  // fade from green to teal
-  for (b = 0; b < 256; b++) { 
-    analogWrite(B_PIN, b);
-    delay(RGB_SPEED);
-  } 
-  // fade from teal to blue
-  for (g = 255; g > 0; g--) { 
-    analogWrite(G_PIN, g);
-    delay(RGB_SPEED);
-  } 
+
+void updateRGB() {
+  ledR.update();
+  ledG.update();
+  ledB.update();
 }
